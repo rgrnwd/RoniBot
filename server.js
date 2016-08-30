51b64d8ac9db65835f1f83db5f8491393eb98409
Peerapat40595 <peerapat40595@gmail.com>
2016-08-30 17:18:29 +0700

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http')
 var url = require('url')
 
 var server = http.createServer(callback)
-server.listen(process.argv[2])
+server.listen(4444, '127.0.0.1')
 
 
 function callback(request, response){
