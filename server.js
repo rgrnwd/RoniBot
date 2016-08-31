bacf3fea782d3e943bceca3edbc7b80e1c14b507
rgrnwd <rgrnwd@thoughtworks.com>
2016-08-31 11:24:55 +0700

    minor refactoring..

--- server.js
+++ server.js
@@ -21,25 +21,24 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.set('port', (process.env.PORT || port));
+// app.set('port', (process.env.PORT || port));
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({
-extended: true
+	extended: true
 }));
 
 app.listen(process.env.PORT || port, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
 
 
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
+app.get('/', function(request, response) {
+	console.log('get received');
+    response.render('index');
 });
 
 app.post('/callback', function(request, response) {
